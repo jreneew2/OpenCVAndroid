7a38a1837cd16c6725bd9aef15a6c7263ee4e0d5
Drew Williams <williams.r.drew@gmail.com>
2016-05-22 12:59:48 -0400

    working on exposure control
    here you go

--- OpenCvTest/app/src/main/java/com/williams/drew/opencvtest/MainActivity.java
+++ OpenCvTest/app/src/main/java/com/williams/drew/opencvtest/MainActivity.java
@@ -1,18 +1,28 @@
 package com.williams.drew.opencvtest;
 
 import android.app.Activity;
+import android.graphics.Camera;
 import android.graphics.Paint;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
 import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.Window;
 import android.view.WindowManager;
 
 import org.opencv.android.JavaCameraView;
@@ -31,21 +41,138 @@ import java.util.Arrays;
 import java.util.concurrent.Semaphore;
 
 
-public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
+public class MainActivity extends AppCompatActivity /*implements CvCameraViewListener2*/ {
 
     private static final String TAG = "OCVSample::Activity";
-
     private CameraBridgeViewBase mOpenCvCameraView;
-
     private boolean mIsJavaCamera = true;
 
     Mat matRGBA;
+
+    private Size mPreviewSize = null;
+    private CameraDevice mCameraDevice = null;
+    private CaptureRequest.Builder mPreviewBuilder = null;
+    private CameraCaptureSession mPreviewSession = null;
+    private long duration = 1000;
+    private int iso = 200;
+
+    private TextureView mTextureView = null;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+            Log.i(TAG, "onSurfaceTextureSizeChanged()");
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            Log.i(TAG, "onSurfaceTextureDestroyed()");
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            Log.i(TAG, "onSurfaceTextureAvailable()");
+
+            CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+            try{
+                String cameraId = manager.getCameraIdList()[0];
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                mPreviewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+
+                try {
+                    manager.openCamera(cameraId, mStateCallback, null);
+                } catch(SecurityException e) {
+                    Log.e(TAG, "could not open camera permission");
+                }
+            }
+            catch(CameraAccessException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    private CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+
+        @Override
+        public void onOpened(CameraDevice camera) {
+            Log.i(TAG, "onOpened");
+            mCameraDevice = camera;
+
+            SurfaceTexture texture = mTextureView.getSurfaceTexture();
+            if(texture == null) {
+                Log.e(TAG, "texture is null");
+                return;
+            }
+
+            texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            Surface surface = new Surface(texture);
+
+            try {
+                mPreviewBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+            mPreviewBuilder.addTarget(surface);
+
+            try {
+                mCameraDevice.createCaptureSession(Arrays.asList(surface), mPreviewStateCallback, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            Log.e(TAG, "onDisconnected");
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            Log.e(TAG, "onError");
+        }
+    };
+
+    private CameraCaptureSession.StateCallback mPreviewStateCallback = new CameraCaptureSession.StateCallback() {
+
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            Log.i(TAG, "onConfigured");
+            mPreviewSession = session;
+
+            mPreviewBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_AE_MODE_OFF);
+            mPreviewBuilder.set(CaptureRequest.SENSOR_EXPOSURE_TIME, duration);
+            mPreviewBuilder.set(CaptureRequest.SENSOR_SENSITIVITY, iso);
+
+            HandlerThread backgroundThread = new HandlerThread("CameraPreview");
+            backgroundThread.start();
+            Handler backgroundHandler = new Handler(backgroundThread.getLooper());
+
+            try {
+                mPreviewSession.setRepeatingRequest(mPreviewBuilder.build(), null, backgroundHandler);
+            } catch(CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(CameraCaptureSession session) {
+            Log.e(TAG, "CameraCaptureSession Configure Failed");
+        }
+    };
 
     public MainActivity() {
         Log.i(TAG, "Instantiated new " + this.getClass());
     }
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+    /*private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
@@ -60,63 +187,74 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                 } break;
             }
         }
-    };
+    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        setContentView(R.layout.show_camera);
+        //setContentView(R.layout.show_camera);
+        setContentView(R.layout.activity_main);
+        mTextureView = (TextureView) findViewById(R.id.textureView1);
+        mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
 
-        mOpenCvCameraView = (JavaCameraView) findViewById(R.id.show_camera_activity_java_surface_view);
+        //mOpenCvCameraView = (JavaCameraView) findViewById(R.id.show_camera_activity_java_surface_view);
 
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        //mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 
-        mOpenCvCameraView.setCvCameraViewListener(this);
+        //mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     @Override
     public void onPause()
     {
         super.onPause();
-        if (mOpenCvCameraView != null)
+        /*if (mOpenCvCameraView != null) {
             mOpenCvCameraView.disableView();
+        }*/
+
+        if(mCameraDevice != null) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
     }
 
     @Override
     public void onResume()
     {
         super.onResume();
-        if (!OpenCVLoader.initDebug()) {
+        /*if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
         } else {
             Log.d(TAG, "OpenCV library found inside package. Using it!");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
+        }*/
     }
 
     public void onDestroy() {
         super.onDestroy();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
+        /*if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();*/
     }
 
-    @Override
+    /*@Override
     public void onCameraViewStarted(int width, int height) {
         matRGBA = new Mat(height, width, CvType.CV_8UC4);
-    }
+    }*/
 
-    @Override
+    /*@Override
     public void onCameraViewStopped() {
         matRGBA.release();
-    }
+    }*/
 
-    @Override
+    /*@Override
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
         matRGBA = inputFrame.rgba();
         return matRGBA;
-    }
+    }*/
 }
--- OpenCvTest/openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ OpenCvTest/openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Development\\Android\\Projects\\OpenCvTest\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\willi\\Documents\\GitHub\\OpenCVAndroid\\OpenCvTest\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
